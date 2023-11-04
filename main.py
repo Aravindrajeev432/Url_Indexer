b9b74d6c09f00edfc24f24e443eab6803cc2df09
Aravind R <aravind.rajeev432@gmail.com>
2023-11-04 21:16:06 +0530

    first

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from decouple import config
+import time
+api_key = config("APIKEY")
+url_list = config("URLLIST", cast=lambda v: [s.strip() for s in v.split(',')])
+
+
+def record_timestamp(result):
+    with open('timestamp.txt', 'a') as f:
+        f.write(time.strftime("%Y-%m-%d %H:%M:%S") + str(result) + '\n')
+
+
+def main() -> bool:
+    
+    url: str = "ssl.bing.com/webmaster/api.svc/json/SubmitUrIbatch"
+    headers: dict = {
+        "Content-Type": "application/json",
+        "charset": "utf-8",
+    }
+    params: dict = {
+        "apikey": api_key,
+    }
+    data: dict = {"siteUrl": "", "urlList": url_list}
+    response = requests.get(url, headers=headers, params=params, json=data)
+    if response.status_code == 200:
+        return True
+    return False
+
+
+record_timestamp(result=main())
---
+++ readme.md
@@ -0,0 +1,48 @@
+# URL Indexer
+
+helps to index url in bing
+
+## Requirements
+
+Python3 and two packages are used in this project
+
+```bash
+  pip install requests
+  pip install python-decouple
+```
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`APIKEY=ADAFadsf**`
+
+`URLLIST=example.com,example2.com,example3.com`
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Aravindrajeev432/Url_Indexer
+```
+
+Go to the project directory
+
+```bash
+  cd Url_Indexer
+```
+
+Run the program
+
+#### Windows OS
+
+```bash
+  python main.py 
+```
+
+#### Mac/Linux
+
+```bash
+    python3 main.py
+```
